43b1faed4bc53794245187d56c1de2d2c7cafaed
takuma <s21c1006@s.chibakoudai.jp>
2022-11-29 19:41:09 +0900

    Initial Commit

---
+++ mypkg/talker.py
@@ -0,0 +1,16 @@
+import rclpy
+from rclpy.node import Node
+from person_msgs.srv import Query
+
+def cb(request, response):
+    if request.name == "安部巧真":
+        response.age = 20
+    else:
+        response.age = 255
+ 
+    return response
+
+rclpy.init()
+node = Node("talker")
+srv = node.create_service(Query, "query", cb)
+rclpy.spin(node)
