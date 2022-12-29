1a87c4040f2746da42b8710d00dbb0a286f923dd
takuma <s21c1006@s.chibakoudai.jp>
2022-12-29 17:06:11 +0900

    Initial Commit

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,31 +1,13 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.srv import Query
+from std_msgs.msg import Int16
 
-def main():
-    rclpy.init()
-    node = Node("listener")
-    client = node.create_client(Query, 'query')
-    while not client.wait_for_service(timeout_sec=1.0): 
-        node.get_logger().info('待機中')
+def cd(msg):
+        global node
+            node.get_logger().info("Listen: %d" % msg.data)
 
-    req = Query.Request()
-    req.name = "安部巧真"
-    future = client.call_async(req)
-    while rclpy.ok():
-        rclpy.spin_once(node)
-        if future.done():
-            try:
-                response = future.result()
-            except:
-                node.get_logger().info('呼び出し失敗')
-            else:
-                node.get_logger().info("age: {}".format(response.age))
+            rclpy.init()
+            node = Node("listener")
+            pub = node.create_subscription(Int16, "countup", cd, 10)
 
-            break
-
-    node.destroy_node()
-    rclpy.shutdown()
-
-if __name__ == '__main__':
-    main()
+            rclpy.spin(node)
