74b433022281d187767464ba040653e837cd10db
takuma <s21c1006@s.chibakoudai.jp>
2022-12-29 17:10:50 +0900

    Initial Commit

--- build/mypkg/build/lib/mypkg/listener.py
+++ build/mypkg/build/lib/mypkg/listener.py
@@ -3,11 +3,11 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 
 def cd(msg):
-        global node
-            node.get_logger().info("Listen: %d" % msg.data)
+    global node
+    node.get_logger().info("Listen: %d" % msg.data)
 
-            rclpy.init()
-            node = Node("listener")
-            pub = node.create_subscription(Int16, "countup", cd, 10)
+rclpy.init()
+node = Node("listener")
+pub = node.create_subscription(Int16, "countup", cd, 10)
 
-            rclpy.spin(node)
+rclpy.spin(node)
