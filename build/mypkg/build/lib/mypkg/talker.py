51c155d9025fc73928abdd679a1444dc8972ab20
takuma <s21c1006@s.chibakoudai.jp>
2022-12-31 19:37:43 +0900

    Initial Commit

--- build/mypkg/build/lib/mypkg/talker.py
+++ build/mypkg/build/lib/mypkg/talker.py
@@ -1,5 +1,6 @@
 # SPDX -FileCopyrightText:2022 Takuma Abe
 # SPDX-License-Identifier: BSD-3-Clause
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
--- test/test.bash
+++ test/test.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX -FileCopyrightText:2022 Takuma Abe
+# SPDX-License-Identifier: BSD-3-Clause
 
 dir=~
 [ "$1" != "" ] && dir="$1"
