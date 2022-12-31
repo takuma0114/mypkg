70c377dc0c9145580a56f8c412f47c46a20a7536
takuma <s21c1006@s.chibakoudai.jp>
2022-12-31 19:08:59 +0900

    Initial Commit

--- install/mypkg/lib/python3.10/site-packages/mypkg/listener.py
+++ install/mypkg/lib/python3.10/site-packages/mypkg/listener.py
@@ -1,13 +1,11 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Int16
+from std_msgs.msg import Int16
 
 def cb(msg):
-    global node
-    node.get_logger().info("Listen: %s" % msg)
+    node.get_logger().info("Listen: %d" % msg.data)
 
 rclpy.init()
 node = Node("listener")
-pub = node.create_subscription(Int16, "person", cb, 10) 
-
- rclpy.spin(node)
+sub = node.create_subscription(Int16, "countup", cb, 10)
+rclpy.spin(node)
